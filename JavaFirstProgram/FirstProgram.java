121843ed771578599b112c3a503ae5b59c8e3152
Anusha498 <ralla.chiru@gmail.com>
2025-07-24 05:02:31 -0700

    FirstProgram.java

---
+++ JavaFirstProgram/FirstProgram.java
@@ -0,0 +1,10 @@
+package JavaFirstProgram;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("This is my first Java program.");
+    }
+
+}
