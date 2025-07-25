d4fa76c72333a65d3fa8d835a6ddc6c02fffdd12
Anusha498 <ralla.chiru@gmail.com>
2025-07-24 22:05:08 -0700

    IncrementDecrement.java

---
+++ Operators/IncrementDecrement.java
@@ -0,0 +1,16 @@
+public class IncrementDecrement {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        System.out.println("Value : " + a);
+        System.out.println("Value: " + b);
+        
+        System.out.println("After Pre incrementing: "+ ++a); //pre increment
+        System.out.println("After post incrementing :"+ a++); //post increment
+
+        System.out.println("After pre Decrementing: "+ ++b); //pre decrement
+        System.out.println("after post Decrementing: " + b++); //post decrement
+
+    } 
+
+}
