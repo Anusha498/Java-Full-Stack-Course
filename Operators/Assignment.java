98dfe80943a27b03b52e3fd41419c8316230ce07
Anusha498 <ralla.chiru@gmail.com>
2025-07-24 21:53:37 -0700

    Assignment.java

---
+++ Operators/Assignment.java
@@ -0,0 +1,23 @@
+public class Assignment {
+    public static void main(String[] args) {
+        //Compound Assignment Operators
+       int num = 10;
+       num += 5; //10+5
+       System.out.println("Number is:"+num);
+
+       num *= 5; //15*5
+       System.out.println("Number is:"+num); 
+
+       num -= 5; //75-5
+       System.out.println("Number is:"+num);
+
+       num /= 5; //70/5
+       System.out.println("Number is:"+num);
+
+       num %= 5; //14%5
+       System.out.println("Number is:"+num);
+
+       
+    }
+
+}
