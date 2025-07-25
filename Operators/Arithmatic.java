4eb45867fca6aef515e67dcab33e406dd7b41f8c
Anusha498 <ralla.chiru@gmail.com>
2025-07-24 21:35:41 -0700

    Arithmatic.java

---
+++ Operators/Arithmatic.java
@@ -0,0 +1,15 @@
+public class Arithmatic {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2  = 5;
+        int sum = num1 + num2; //addition operation
+        System.out.println("Sum:"+sum);
+        
+        
+    
+        System.out.println("Difference:" + (num1 - num2));
+        System.out.println("Product:" + (num1 * num2));
+        System.out.println("Quotient:" + (num1 / num2));
+        System.out.println("Remainder:" + (num1 % num2));
+    }
+}
