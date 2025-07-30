91687307fba24bd1c901306403aa9901bf743d2b
Anusha498 <ralla.chiru@gmail.com>
2025-07-30 02:34:58 -0700

    Demo2.java

---
+++ ControlStatements/Demo2.java
@@ -0,0 +1,17 @@
+public class Demo2 {
+    public static void main(String[] args){
+        int age = 22;
+        boolean id = true;
+        
+        
+        if(age >= 21){
+            if(id ==  true){
+               
+             System.out.println("Can Enter");
+        } else {
+            System.out.println("Cannot Enter without ID");
+        }
+    }
+    
+    }
+}
