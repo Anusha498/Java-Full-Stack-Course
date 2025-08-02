e180c43f39bd3c3d67574b012fc21316ed4a6f28
Anusha498 <ralla.chiru@gmail.com>
2025-08-02 05:28:11 -0700

    LoopsDemo.java

---
+++ Loops/LoopsDemo.java
@@ -0,0 +1,12 @@
+public class LoopsDemo {
+    public static void main (String[] args){
+    for(int i=0; i<=5;i++){
+        //skip when i is 3
+        if(i == 3) {
+            continue;
+        }
+        System.out.println(i);
+    } 
+    }
+
+}
