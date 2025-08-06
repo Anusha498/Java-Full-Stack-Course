202889e74924680fe2dae204a5739078cd014145
Anusha498 <ralla.chiru@gmail.com>
2025-08-06 11:01:58 +0530

    Age.java

--- Loops/Age.java
+++ Loops/Age.java
@@ -11,16 +11,31 @@ public class Age{
         }
    
      //read text
-    System.out.println("What is your name:");
-    String name = sc.next();
-    System.out.println("Welcome:"+name);
+    //System.out.println("What is your name:");
+    //String name = sc.next();
+    //System.out.println("Welcome:"+name);
 
     //Simulate while loop
-    System.out.println("Welcome,Do you want to continue? (yes/no)");
-    String user_choice = sc.next();
-    while (user_choice.equals("yes")) {
-        System.out.println("Do this work");
+    //System.out.println("Welcome,Do you want to continue? (yes/no)");
+    //String user_choice = sc.next();
+    //while (user_choice.equals("yes")) {
+      //  System.out.println("Do this work");
 
-     }
+    // }
+    //while
+    int count = 1;
+    while (count <= 5) {
+            System.out.println("Hi: " + count);
+            count++;
+        }
+        
+        // Simulate do-while loop
+        
+        do {
+            System.out.println("Hi: " +count);
+           count++;
+        } while (count<=4 );
+        
+        sc.close();
     }
 }
